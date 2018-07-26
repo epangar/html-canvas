c9dab4263de7a5b4b1e9e0f0fb548438fa119394
epangar <e.pan.gar@gmail.com>
2018-07-26 14:23:03 +0200

    Update main.js

--- 1/main.js
+++ 1/main.js
@@ -20,10 +20,8 @@
         ctx.clearRect(210,20,30,20)
         // Clear a rectangle of width 30 and height 20 pixels at (260,20) 
         ctx.clearRect(260,20,30,20)
+     
         
-        
-
-
         // Drawing complex shapes 
         // Filled triangle
 
@@ -40,5 +38,37 @@
         ctx.lineTo(40,190)
         ctx.closePath()
         ctx.stroke()
+
+        // A more complex set of lines...
+        ctx.beginPath()
+        ctx.moveTo(160,160)
+        ctx.lineTo(170,220)
+        ctx.lineTo(240,210)
+        ctx.lineTo(260,170)
+        ctx.lineTo(190,140)
+        ctx.closePath()
+        ctx.stroke()
+
+        // Drawing arcs
+        // Drawing a semicircle 
+
+        ctx.beginPath()
+        // Draw an arc at (400,50) with radius 40 from 0 to 180 degrees,anticlockwise
+        ctx.arc(100,300,40,0,Math.PI,true)
+        ctx.stroke()
+
+        // Drawing a full circle 
+        ctx.beginPath()
+        // Draw an arc at (500,50) with radius 30 from 0 to 360 degrees,anticlockwise 
+        ctx.arc(100,300,30,0,2*Math.PI,true)
+        ctx.fill()
+
+        // Drawing a three-quarter arc 
+        ctx.beginPath()
+        // Draw an arc at (400,100) with radius 25 from 0 to 270 degrees,clockwise 
+        ctx.arc(200,300,25,0,1.5*Math.PI,false)
+        ctx.stroke()
+        
+        
     } 
   
